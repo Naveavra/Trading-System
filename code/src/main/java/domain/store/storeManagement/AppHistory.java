2bae36d4cac58c6683230429bfd5daf7644813b6
Nave Avraham <naveavr@post.bgu.ac.il>
2023-04-11 14:46:19 +0300

    app history first version

--- code/src/main/java/domain/store/storeManagement/AppHistory.java
+++ code/src/main/java/domain/store/storeManagement/AppHistory.java
@@ -1,37 +1,86 @@
 package domain.store.storeManagement;
 
+import datastructres.Pair;
 import utils.Role;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+
 
 public class AppHistory {
-    int creatorID;
-    int storeID;
-    public AppHistory(int creatorID, int storeID)
-    {
-        this.creatorID = creatorID;
-        this.storeID = storeID;
+    public static class Node{
+        Pair<Integer, Role> data; //userid and role
+        ArrayList<Node> children; //list of all the users this user appoint in this store
+        public Node(Pair<Integer, Role> appointment){
+            //Assign data to the new node, set left and right children to null
+            this.data = appointment;
+            this.children = new ArrayList<Node>();
+        }
+        private Node findNode(Integer userId)
+        {
+            if (Objects.equals(data.getFirst(), userId))
+            {
+                return this;   //meaning this user is already part of the store
+            }
+            for (Node child : children)
+            {
+                Node node = child.findNode(userId);
+                if (node != null)
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
 
+        private void addChild(Pair<Integer, Role> child)
+        {
+            children.add(new Node((child)));
+        }
+
+        //remove the node and all of his descendants
+        private ArrayList<Integer> deleteNode(Pair<Integer, Role> child)
+        {
+            ArrayList<Integer> dismissed = new ArrayList<>();
+            Node childNode = findNode(child.getFirst());
+            if (childNode == null) {
+                return null; // child not found
+            }
+            children.remove(childNode);
+            for (Node grandchild : childNode.children) {
+                dismissed.add(grandchild.data.getFirst());
+                childNode.deleteNode(grandchild.data);
+            }
+            return dismissed;
+        }
+    }
+    //Represent the root of binary tree
+    public Node root;
+
+    public AppHistory(Pair<Integer, Role> creatorNode){
+
+        root = new Node(creatorNode);
+    }
+    public void addNode(Integer father, Pair<Integer, Role> child) throws Exception {
+        Node childNode = root.findNode(child.getFirst());
+        Node fatherNode = root.findNode(father);
+        if (childNode != null)
+        {
+            throw new Exception("user already have a role in the store");
+        }
+        if (fatherNode == null)
+        {
+            throw new Exception("User cant appoint other users in the store");
+        }
+        fatherNode.addChild(child);
     }
 
-//    public List<AppHistory> appointed;
-//    public AppHistory appointedMe;
-//    public Role role;
-//    public int appointedUserId;
-//    public AppHistory(AppHistory appointedMe, int appointedUserId, Role role){
-//        this.appointedUserId = appointedUserId;
-//        this.role = role;
-//        appointed = new LinkedList<>();
-//    }
-//
-//    //in case a user appoints another user to the store
-//    public void addAppointed(int appointee, int appointed, Role role){
-//
-//    }
-//
-//    //remove the manager and all of his appointed if needed
-//    public void removeAppointed(int appointed){
-//
-//    }
+    public ArrayList<Integer> removeChild(Integer userId) throws Exception {
+       Node childNode = root.findNode(userId);
+       if (childNode == null)
+       {
+           throw new Exception("user isn't part of the store");
+       }
+       return root.deleteNode(childNode.data);
+    }
 }
