9a0f422dad8a3fd6621af19f5ae15be4e46ed54f
mikid <daniarov@post.bgu.ac.il>
2023-04-12 04:25:03 +0300

    Added comments

--- code/src/main/java/domain/store/order/OrderController.java
+++ code/src/main/java/domain/store/order/OrderController.java
@@ -1,10 +1,68 @@
 package domain.store.order;
 
 import domain.store.order.Order;
+import utils.Status;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OrderController {
 
     private ConcurrentHashMap<Integer, Order> orders;
+    private AtomicInteger orderID;
+    
+    public OrderController(){
+        orders = new ConcurrentHashMap<Integer,Order>();
+        orderID = new AtomicInteger();
+    }
+    
+    public synchronized void createNewOrder(int userID,HashMap<Integer,HashMap<Integer,Integer>> products){
+        int id = orderID.getAndIncrement();
+        orders.put(id, new Order(id,userID,products));
+    }
+    
+    public synchronized Order getOrder(int order_ID){
+        if(orders.containsKey(order_ID)){
+            return orders.get(order_ID);
+        }
+        return null;
+    }
+
+    //only a specific user will be the cause of calling this function, so no need to synchronize
+    public void replaceProductsInOrder(int order_ID,int store_ID,HashMap<Integer,Integer> products){
+        Order ord;
+        if((ord = getOrder(order_ID)) != null){
+            ord.replaceProductsInOrder(store_ID, products);
+        }
+    }
+    //only a specific user will be the cause of calling this function, so no need to synchronize
+    public void addProductsToOrder(int order_ID,int store_ID,HashMap<Integer,Integer> products){
+        Order ord;
+        if((ord = getOrder(order_ID)) != null){
+            ord.addProductsToOrder(store_ID, products);
+        }
+    }
+    
+    /**
+     * need to check whether the return value is null.
+     * @param order_ID
+     * @return order Status or Null
+     */
+    public Status getOrderStatus(int order_ID){
+        Order ord;
+        if((ord = getOrder(order_ID)) != null){
+            return ord.getStatus();
+        }
+        return null;
+    }
+
+    public void changeStatus(int order_ID,Status status){
+        Order ord;
+        if((ord = getOrder(order_ID)) != null){
+            ord.setStatus(status);
+        }
+    }
+
+
 }
--- code/src/main/java/domain/store/product/Product.java
+++ code/src/main/java/domain/store/product/Product.java
@@ -1,13 +1,77 @@
 package domain.store.product;
 
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Product {
+    public final int id;
+    private List<String> categories;
+    public String name;
+    public String description;
+    public int quantity; //maybe not neccessary here
+    public int price; //for one product
+    //private ConcurrentLinkedDeque<String> categories;
+    //private double discount;  no need for discount here, the discount is calculated by the policy
+    
+    /**
+     * Quantity, Price and Categories are set manually.
+     * @param prod_id Unique
+     * @param _name String
+     * @param desc String
+     */
+    public Product(int prod_id, String _name, String desc){
+        id = prod_id;
+        name = _name;
+        description = desc;
+        quantity = 0;
+        price = 0;
+        categories = new ArrayList<>();
+    }
+    public void addCategory(String _category){
+        if(!categories.contains(_category)){
+            categories.add(_category);
+        }
+    }
 
-    private ConcurrentLinkedDeque<String> categories;
-    private String name;
-    private int quantity;
-    private int price;
-    private double discount;
+    public List<String> getCategories(){
+        return this.categories;
+    }
 
+    private void setCategories(List<String> categories){
+        this.categories = categories;
+    }
+
+    public void setQuantity(int amount){
+        if(amount>0){
+            quantity = amount;
+        }
+        //else maybe should throw exception
+    }
+    
+    public void setPrice(int newPrice){
+        if(newPrice>0){
+            price = newPrice;
+        }
+        //else maybe should throw exception
+    }
+     
+    /**
+     * This function returns a deep copy of the product, 
+     * should be used when inserting products to the store Inventory.
+     * @return new deep copy of the product.
+     */
+    public Product clone(){
+        Product clone = new Product(id,name,description);
+        clone.setPrice(price);
+        clone.setQuantity(quantity);
+        for(String cat: categories){
+            clone.addCategory(cat);
+        }
+        return clone;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+    
 }
