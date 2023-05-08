b5f74206466a8b0cecb5e4cb61105dd670ab2d86
Elibs1 <elibenshimol6@gmail.com>
2023-05-08 11:38:23 +0300

    adding server

--- Trading-Api/code/src/main/java/server/Server.java
+++ Trading-Api/code/src/main/java/server/Server.java
@@ -3,17 +3,27 @@ package server;
 import market.Admin;
 import market.Market;
 import org.json.JSONObject;
+import spark.Spark;
 import utils.Token;
 import utils.marketRelated.Response;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static spark.Spark.*;
 
 public class Server {
+    public static Market market = new Market(new Admin(1, "hi@co.il", "123Aaa"));
+    static ConnectedThread connectedThread ;
+    static ConcurrentHashMap<Integer,Boolean> connected = new ConcurrentHashMap<>();
     public static void main(String[] args) {
-        Market market = new Market(new Admin(1, "hi@co.il", "123Aaa"));
         market.register("eli@gmail.com","aA12345","22/02/2002");
+        //Spark.webSocket("/api/login", MainWebSocket.class);
+        //Spark.webSocket("/api/member",  MemberWebSocket.class);
+        init();
+        connectedThread = new ConnectedThread(connected);
+        connectedThread.start();
         before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 
         options("/*",
@@ -37,31 +47,57 @@ public class Server {
                 });
 
         post("/api/auth/login", (req, res) -> {
-            System.out.println(req);
-            System.out.println(req.body());
-            System.out.println(req.headers());
-            System.out.println(req.userAgent());
             JSONObject request = new JSONObject(req.body());
             String email = request.get("email").toString();
             String pass  = request.get("password").toString();
             LinkedList<String> answers = new LinkedList<>();
             Response<Token> r = market.login(email,pass,answers);
-           // System.out.println(r.getValue().getUserName());
+            // System.out.println(r.getValue().getUserName());
             JSONObject json = new JSONObject();
             if(r.getErrorMessage()==null) {
+                System.out.println("new user comes in , id: " + r.getValue().getUserId());
                 json.put("token", r.getValue().getToken());
                 json.put("userId", r.getValue().getUserId());
                 json.put("userName", r.getValue().getUserName());
+                json.put("isAdmin",false);
+                connected.put( r.getValue().getUserId() ,true);
                 System.out.println(json);
                 res.status(200);
                 res.body(json.toString());
             }
             else {
-                 json.put("errorMsg", r.getErrorMessage());
-                 res.status(400);
+                json.put("errorMsg", r.getErrorMessage());
+                res.status(400);
                 res.body(json.toString());
             }
-                return res.body();
+            return res.body();
         });
+        post("/api/auth/ping" ,(req,res)->{
+            JSONObject request = new JSONObject(req.body());
+            String id = request.get("userId").toString();
+            connected.put(Integer.parseInt(id), true);
+            System.out.println(id + " becomes true");
+            res.status(200);
+            res.body("ping success");
+            return res.body();
+        });
+        post("/api/auth/guest/enter", (req, res) -> {
+            Response<Integer> r = market.enterGuest();
+            JSONObject json = new JSONObject();
+            if(r.getErrorMessage()==null) {
+                System.out.println("new user comes in , id: " + r.getValue());
+                res.status(200);
+                json.put("guestId", r.getValue());
+                connected.put(r.getValue(),true);
+                res.body(json.toString());
+            }
+            else{
+                res.status(400);
+                json.put("errorMsg", r.getValue());
+                res.body(json.toString());
+            }
+            return res.body();
+        });
+
     }
 }
